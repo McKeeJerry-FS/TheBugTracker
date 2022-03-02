3308fdfff7cc9ecd6556eb35c2635f15d3d3463c
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-02 11:22:31 -0500

    updating dependencies
    Added code for use of a PostgreSQL Database; created Scaffolding for Identity; began modifying the Registration page

--- Startup.cs
+++ Startup.cs
@@ -28,8 +28,9 @@ namespace TheBugTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
