9dd6b52c662a5ccfce88ffdc2cb55a22289470b6
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-03 11:11:14 -0500

    Update ApplicationDbContext.cs
    - creating DbSet<> properties  for each model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,5 +13,21 @@ namespace TheBugTracker.Data
             : base(options)
         {
         }
+
+        public DbSet<BTCompany> Companies { get; set; }
+        public DbSet<BTInvite> Invites { get; set; }
+        public DbSet<BTProject> Projects { get; set; }
+        public DbSet<BTTicket> Tickets { get; set; }
+        public DbSet<BTNotification> Notifications { get; set; }
+        public DbSet<BTProjectPriority> ProjectPriorities { get; set; }
+        public DbSet<BTTicketAttachment> TicketAttachments { get; set; }
+        public DbSet<BTTicketComment> TicketComments { get; set; }
+        public DbSet<BTTicketHistory> TicketHistories { get; set; }
+        public DbSet<BTTicketPriority> TicketPriorities { get; set; }
+        public DbSet<BTTicketStatus> TicketStatuses { get; set; }
+        public DbSet<BTTicketType> TicketTypes { get; set; }
+
+
+
     }
 }
