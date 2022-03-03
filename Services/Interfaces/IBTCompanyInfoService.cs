71a490da4d4ea51a985735c7ab50cd9660199717
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-03 15:34:03 -0500

    Create IBTCompanyInfoService Interface

---
+++ Services/Interfaces/IBTCompanyInfoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTCompanyInfoService
+    {
+        public Task<BTCompany> GetCompanyInfoByIdAsync(int? companyId);
+        public Task<BTUser> GetAllMembersAsync(int companyId);
+        public Task<BTProject> GetAllProjectsAsync(int companyId);
+        public Task<BTTicket> GetAllTicketsAsync(int companyId);
+
+    }
+}
