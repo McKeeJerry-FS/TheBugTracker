51977c002f7667548851ee3493d680713f667eec
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-03 15:35:48 -0500

    Create BTCompanyInfoService Class

---
+++ Services/BTCompanyInfoService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTCompanyInfoService : IBTCompanyInfoService
+    {
+        public Task<BTUser> GetAllMembersAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BTProject> GetAllProjectsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BTTicket> GetAllTicketsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BTCompany> GetCompanyInfoByIdAsync(int? companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
