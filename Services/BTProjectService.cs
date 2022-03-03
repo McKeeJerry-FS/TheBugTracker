f7c2a0a03f6f9c1fa29b55e16c755b9414a8ff53
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-03 17:40:23 -0500

    create ProjectService

--- Services/BTProjectService.cs
+++ Services/BTProjectService.cs
@@ -2,10 +2,116 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
 
 namespace TheBugTracker.Services
 {
-    public class BTProjectService
+    public class BTProjectService : IBTProjectService
     {
+        public Task AddNewProjectAsync(BTProject project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddProjectManagerAsync(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddUserToProjectAsync(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ArchiveProjectAsync(BTProject project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetAllProjectMembersExceptPMAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTProject>> GetAllProjectsByCompany(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTProject>> GetAllProjectsByPriority(int companyId, string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTProject>> GetArchivedProjectsByCompany(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetDevelopersOnProjectAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BTProject> GetProjectByIdAsync(int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BTUser> GetProjectManagerAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetProjectMembersByRoleAsync(int projectId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetSubmittersOnProjectAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTProject>> GetUserProjectsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersNotOnProjectAsync(int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserOnProject(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> LookupProjectPriorityId(string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveProjectManagerAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveUserFromProjectAsync(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveUsersFromProjectByRoleAsync(string role, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProjectAsync(BTProject project)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Services/Interfaces/IBTProjectService.cs
+++ Services/Interfaces/IBTProjectService.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Models;
 
 namespace TheBugTracker.Services.Interfaces
 {
     public interface IBTProjectService
     {
+        public Task AddNewProjectAsync(BTProject project);
+        public Task<bool> AddProjectManagerAsync(string userId, int projectId);
+        public Task<bool> AddUserToProjectAsync(string userId, int projectId);
+        public Task ArchiveProjectAsync(BTProject project);
+        public Task<List<BTProject>> GetAllProjectsByCompany(int companyId);
+        public Task<List<BTProject>> GetAllProjectsByPriority(int companyId, string priorityName);
+        public Task<List<BTUser>> GetAllProjectMembersExceptPMAsync(int projectId);
+        public Task<List<BTProject>> GetArchivedProjectsByCompany(int companyId);
+        public Task<List<BTUser>> GetDevelopersOnProjectAsync(int projectId);
+        public Task<BTUser> GetProjectManagerAsync(int projectId);
+        public Task<List<BTUser>> GetProjectMembersByRoleAsync(int projectId, string role);
+        public Task<BTProject> GetProjectByIdAsync(int projectId, int companyId);
+        public Task<List<BTUser>> GetSubmittersOnProjectAsync(int projectId);
+        public Task<List<BTUser>> GetUsersNotOnProjectAsync(int projectId, int companyId);
+        public Task<List<BTProject>> GetUserProjectsAsync(string userId);
+        public Task<bool> IsUserOnProject(string userId, int projectId);
+        public Task<int> LookupProjectPriorityId(string priorityName);
+        public Task RemoveProjectManagerAsync(int projectId);
+        public Task RemoveUsersFromProjectByRoleAsync(string role, int projectId);
+        public Task RemoveUserFromProjectAsync(string userId, int projectId);
+        public Task UpdateProjectAsync(BTProject project);
+
+
+
+
+
+
+
+
     }
 }
