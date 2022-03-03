3c4c68e9ee11541287e68b40198ad8a42fc7b788
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-03 14:52:36 -0500

    create BTRoleService

---
+++ Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+        
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+
+        
+        public Task<List<BTUser>> GetUsersInRolesAsync(string roleName, int companyId);
+
+        public Task<List<BTUser>> GetUsersNotInRolesAsync(string roleName, int companyId);
+
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+
+    }
+}
