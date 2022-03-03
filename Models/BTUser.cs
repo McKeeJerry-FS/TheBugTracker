a42c3c02c5ba54052416d5526da0579043a2b5e6
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-03 09:14:28 -0500

    Update BTUser.cs
    added new properties to BTUser class

--- Models/BTUser.cs
+++ Models/BTUser.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -22,8 +24,24 @@ namespace TheBugTracker.Models
         [Display(Name = "Full Name")]
         public string FullName { get { return $"{FirstName} {LastName}"; } }
 
+        // Avatar properties
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [DisplayName("Avatar")]
+        public string AvatarFileName { get; set; }
+
+        public byte[] AvatarFileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string AvatarContentType { get; set; }
 
 
+        public int? CompanyId { get; set; } //Foreign Key
 
+        //Naigation properties
+        public virtual BTCompany Company { get; set; }
+        public virtual ICollection<BTProject> Projects { get; set; }
     }
 }
