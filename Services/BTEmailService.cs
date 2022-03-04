b50bb09902d16a52e3261b78e7a8a484e46c3e76
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-04 15:22:15 -0500

    Create MailSettings Class

--- Services/BTEmailService.cs
+++ Services/BTEmailService.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Models;
 
 namespace TheBugTracker.Services
 {
-    public class BTEmailService
+    public class BTEmailService : IEmailSender
     {
+        // Constructor
+        public BTEmailService(IOptions<MailSettings> mailSettings)
+        {
+
+        }
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
