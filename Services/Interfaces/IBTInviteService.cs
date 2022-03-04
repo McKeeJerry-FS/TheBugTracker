5c333f2137725071aa4b74412e51a60d3f8adb73
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-04 17:17:14 -0500

    Update IBTInviteService.cs
    - built out BTInviteService Interface

--- Services/Interfaces/IBTInviteService.cs
+++ Services/Interfaces/IBTInviteService.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Models;
 
 namespace TheBugTracker.Services.Interfaces
 {
     public interface IBTInviteService
     {
+        public Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId);
+        public Task AddNewInviteAsync(BTInvite invite);
+        public Task<bool> AnyInviteAsync(Guid token, string email, int companyId);
+        public Task<BTInvite> GetInviteAsync(int inviteId, int companyId);
+        public Task<BTInvite> GetInviteASync(Guid token, string email, int companyId);
+        public Task<bool> ValidateInviteCodeAsync(Guid? token);
     }
 }
