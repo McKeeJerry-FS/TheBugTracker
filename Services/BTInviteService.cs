84e28b4be1b1221154c067f3fde5867cb84875d9
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-04 17:06:16 -0500

    Update BTInviteService.cs
    - Setup for Implementation Method

--- Services/BTInviteService.cs
+++ Services/BTInviteService.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Data;
 
 namespace TheBugTracker.Services
 {
     public class BTInviteService
     {
+        private readonly ApplicationDbContext _context;
+
+        // Dependency Injection
+        public BTInviteService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
     }
 }
