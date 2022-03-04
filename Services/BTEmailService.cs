974573e7da6982201674c53044c1438379d72ea4
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-04 15:41:26 -0500

    Created BTEmailService

--- Services/BTEmailService.cs
+++ Services/BTEmailService.cs
@@ -1,23 +1,56 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
+using MailKit.Net.Smtp;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TheBugTracker.Models;
+using MailKit.Security;
 
 namespace TheBugTracker.Services
 {
     public class BTEmailService : IEmailSender
     {
+        private readonly MailSettings _mailSettings;
+
         // Constructor
         public BTEmailService(IOptions<MailSettings> mailSettings)
         {
-
+            _mailSettings = mailSettings.Value;
         }
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        
+        public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            MimeMessage email = new();
+
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailTo));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder
+            {
+                HtmlBody = htmlMessage
+            };
+
+            email.Body = builder.ToMessageBody();
+
+            try
+            {
+                using var smtp = new SmtpClient();
+                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+
+                await smtp.SendAsync(email);
+
+                smtp.Disconnect(true);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
