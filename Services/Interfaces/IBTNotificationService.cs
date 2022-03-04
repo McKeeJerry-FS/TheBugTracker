a406132c315d2e009a0ec9bcaa83c88e4c8fb273
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-04 16:10:48 -0500

    Update IBTNotificationService.cs
    - created Interface

--- Services/Interfaces/IBTNotificationService.cs
+++ Services/Interfaces/IBTNotificationService.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Models;
 
 namespace TheBugTracker.Services.Interfaces
 {
-    interface IBTNotificationService
+    public interface IBTNotificationService
     {
+        public Task AddNotificationAsync(BTNotification notification);
+        public Task<List<BTNotification>> GetRecievedNotificationsAsync(string userId);
+        public Task<List<BTNotification>> GetSentNotificationsAsync(string userId);
+        public Task SendEmailNotificationsByRoleAsync(BTNotification notification, int companyId, string role);
+        public Task SendMembersEmailNotificationAsync(BTNotification notification, List<BTUser> members);
+        public Task SendEmailNotificationAsync(BTNotification notification, string emailSbject);
+
     }
 }
