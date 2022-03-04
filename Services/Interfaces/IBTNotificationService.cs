e381477c816ab169c08af48bf2d02166a8b005c5
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-04 16:12:15 -0500

    Update IBTNotificationService.cs

--- Services/Interfaces/IBTNotificationService.cs
+++ Services/Interfaces/IBTNotificationService.cs
@@ -13,7 +13,7 @@ namespace TheBugTracker.Services.Interfaces
         public Task<List<BTNotification>> GetSentNotificationsAsync(string userId);
         public Task SendEmailNotificationsByRoleAsync(BTNotification notification, int companyId, string role);
         public Task SendMembersEmailNotificationAsync(BTNotification notification, List<BTUser> members);
-        public Task SendEmailNotificationAsync(BTNotification notification, string emailSbject);
+        public Task SendEmailNotificationAsync(BTNotification notification, string emailSubject);
 
     }
 }
