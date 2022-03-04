810d2eed2282081c0d986908d6d5b59547efde8d
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-03-04 08:24:53 -0500

    Update BTTicketService.cs
    - Implemented Ticket Service

--- Services/BTTicketService.cs
+++ Services/BTTicketService.cs
@@ -2,10 +2,115 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
 
 namespace TheBugTracker.Services
 {
-    public class BTTicketService
+    public class BTTicketService : IBTTicketService
     {
+        //CRUD - CREATE
+        public Task AddNewTicketAsync(BTTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        //CRUD - DELETE
+        public Task ArchiveTicketAsync(BTTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AssignTicketAsync(int ticketId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetAllTicketsByCompanyAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetAllTicketsByPriorityAsync(int companyId, string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetAllTicketsByStatusAsync(int companyId, string statusName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetAllTicketsByTypeAsync(int companyId, string typeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetArchivedTicketsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetProjectTicketsByPriorityAsync(string priorityName, int companyId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetProjectTicketsByRoleAsync(string role, string userId, int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetProjectTicketsByStatusAsync(string statusName, int companyId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetProjectTicketsByTypeAsync(string typeName, int companyId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        //CRUD - READ
+        public Task<BTTicket> GetTicketByIdAsync(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BTUser> GetTicketDeveloperAsync(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetTicketsByRoleAsync(string role, string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTTicket>> GetTicketsByUserIdAsync(string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketPriorityIdAsync(string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketStatusIdAsync(string statusName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketTypeIdAsync(string typeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        //CRUD - UPDATE
+        public Task UpdateTicketAsync(BTTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
